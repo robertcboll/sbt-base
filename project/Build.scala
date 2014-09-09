2baa98fb61eb95d66941f822e569242573c356c8
Robert Boll <robertcboll@gmail.com>
2014-09-09 16:52:42 -0400

    .scala build definition

---
+++ project/Build.scala
@@ -0,0 +1,29 @@
+import sbt._
+import Keys._
+
+object Build extends sbt.Build {
+  
+  override lazy val settings = super.settings ++
+    Seq(
+      organization := "sh.robb",
+      name := "sbt-plugins"
+    )
+
+  lazy val root = Project(id = "sbt-plugins", base = file("."))
+    .settings(sbtPlugin := true)
+    .settings(Dependencies.plugins: _*)
+
+
+  object Dependencies {
+
+    lazy val plugins = Seq(
+        addSbtPlugin("com.typesafe.sbt" % "sbt-git" % "0.6.4"), 
+        addSbtPlugin("com.eed3si9n" % "sbt-unidoc" % "0.3.1"), 
+        addSbtPlugin("com.typesafe.sbt" % "sbt-site" % "0.8.1"), 
+        addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "0.7.5-RC1"), 
+        addSbtPlugin("de.johoop" % "jacoco4sbt" % "2.1.6"), 
+        addSbtPlugin("io.spray" % "sbt-revolver" % "0.7.2")
+      )
+  }
+}
+
