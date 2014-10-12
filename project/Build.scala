00514c5758f65a0473de44d75fa146b58d7a61e3
Robert Boll <robertcboll@gmail.com>
2014-10-12 12:09:11 -0400

    back to recent version

--- project/Build.scala
+++ project/Build.scala
@@ -27,7 +27,7 @@ object Build extends sbt.Build {
       addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "0.8.0-M2"), 
       addSbtPlugin("de.johoop" % "jacoco4sbt" % "2.1.6"), 
       addSbtPlugin("io.spray" % "sbt-revolver" % "0.7.2"),
-      addSbtPlugin("net.databinder.giter8" % "giter8-scaffold" % "0.6.2")  
+      addSbtPlugin("net.databinder.giter8" % "giter8-scaffold" % "0.6.6")  
     )
   }
 }
