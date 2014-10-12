f96a8a2f82d4fcf4a51c94b7537ebb2257c9e97e
Robert Boll <robertcboll@gmail.com>
2014-10-12 11:30:04 -0400

    versions

--- project/Build.scala
+++ project/Build.scala
@@ -24,10 +24,10 @@ object Build extends sbt.Build {
       addSbtPlugin("com.typesafe.sbt" % "sbt-git" % "0.6.4"), 
       addSbtPlugin("com.eed3si9n" % "sbt-unidoc" % "0.3.1"), 
       addSbtPlugin("com.typesafe.sbt" % "sbt-site" % "0.8.1"), 
-      addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "0.7.5"), 
+      addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "0.8.0-M2"), 
       addSbtPlugin("de.johoop" % "jacoco4sbt" % "2.1.6"), 
       addSbtPlugin("io.spray" % "sbt-revolver" % "0.7.2"),
-      addSbtPlugin("net.databinder.giter8" % "giter8-scaffold" % "0.6.4")  
+      addSbtPlugin("net.databinder.giter8" % "giter8-scaffold" % "0.6.6")  
     )
   }
 }
