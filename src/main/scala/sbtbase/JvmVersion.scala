2df98ca384722710d8ba0120f45907207303beb8
Robert Boll <robertcboll@gmail.com>
2014-12-16 13:34:45 -0500

    there we go

--- src/main/scala/sbtbase/JvmVersion.scala
+++ src/main/scala/sbtbase/JvmVersion.scala
@@ -13,15 +13,18 @@ object JvmVersion {
     
     val runtimeVersion = sys.props.get("java.version")
     println("the runtimeVersion is " + runtimeVersion)
+    
     runtimeVersion match {
-      case v if v.startsWith("1.7") => 
-        println("setting to common only")
-        common
-      case v if v.startsWith("1.8") => 
-        println("setting to doclint plus common")
-        common ++ Seq(
-          javacOptions in (Compile, doc) += "-Xdoclint:none"
-        )
+      case Some(vers) =>
+        vers match {
+          case v if v.startsWith("1.7") => 
+            println("setting to common only")
+            common
+          case v if v.startsWith("1.8") => 
+            println("setting to doclint plus common")
+            common ++ Seq(javacOptions in (Compile, doc) += "-Xdoclint:none")
+        }
+      case _ => common
     }
   }
 }
