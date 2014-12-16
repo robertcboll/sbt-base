00549bf97e4391961f556f1ddf06fffa1dba7509
Robert Boll <robertcboll@gmail.com>
2014-12-16 16:20:56 -0500

    adding findbugs and checkstyle

--- src/main/scala/sbtbase/Settings.scala
+++ src/main/scala/sbtbase/Settings.scala
@@ -12,13 +12,15 @@ object Settings {
   }
 
   def all(lang: Language) = {
+    val common = Tools.style ++ Tools.findbugs
+
     lang match {
-      case Java => Seq(
+      case Java => common ++ Seq(
         autoScalaLibrary := false,
         crossPaths := false,
         javacOptions in (Compile, compile) ++= Seq("-Xlint:unchecked", "-Xlint:deprecation")
       )
-      case Scala => Seq(
+      case Scala => common ++ Seq(
         scalacOptions := Seq("-encoding", "UTF-8", "-deprecation", "-feature", "-unchecked", "-Xlint")
       )
     }
