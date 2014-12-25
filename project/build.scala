08bab747802ecdc58f0be579620225f0380d29d5
Robert Boll <robertcboll@gmail.com>
2014-12-25 14:45:19 -0500

    validate old assumption

--- project/build.scala
+++ project/build.scala
@@ -17,7 +17,7 @@ object Build extends sbt.Build {
 
   lazy val root = Project(id = "sbt-base", base = file("."))
     .settings(sbtPlugin := true)
-    .settings(dependencyOverrides += "org.clapper" %% "scalasti" % "2.0.0")
+    //.settings(dependencyOverrides += "org.clapper" %% "scalasti" % "2.0.0")
     .settings(Dependencies.plugins: _*)
 
   object Dependencies {
@@ -28,14 +28,19 @@ object Build extends sbt.Build {
       addSbtPlugin("com.typesafe.sbt" % "sbt-git" % "0.6.4"),
       addSbtPlugin("net.databinder.giter8" % "giter8-scaffold" % "0.6.6"),
 
+      /* docs and site */
       addSbtPlugin("com.eed3si9n" % "sbt-unidoc" % "0.3.1"),
       addSbtPlugin("com.typesafe.sbt" % "sbt-site" % "0.8.1"),
 
+      /* packaging */
       addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "0.8.0-RC2"),
 
+      /* analysis tools */
       addSbtPlugin("de.johoop" % "jacoco4sbt" % "2.1.6"),
       addSbtPlugin("de.johoop" % "findbugs4sbt" % "1.3.0"), // not java8 compatible
       addSbtPlugin("com.etsy" % "sbt-checkstyle-plugin" % "0.4.1")
+    
+      /* need to override scalasti to 2.0 to avoid asm conflict with findbugs */
     )
   }
 }
